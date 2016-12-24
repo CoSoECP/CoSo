69694476c51a4f48e499ac9cb0fd1acaf10a922e
Melanie Berard <melanie.berard@student.ecp.fr>
2016-12-24 10:17:29 +0000

    Create a list of candidates

--- coso/polls/views.py
+++ coso/polls/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,3 +7,8 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def import_static_files(request):
+    json_data = open('./static/french_politicians.json')
+    data = json.load(json_data)
+    print data
+    json_data.close()
