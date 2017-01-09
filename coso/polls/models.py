8370aa62421f186de2d09783e3a4768ee7c67cb4
Nicolas Parent <nicolas.parent@student.ecp.fr>
2017-01-09 11:24:22 +0100

    Replace Polls by Trends. Correct some mistakes

--- coso/polls/models.py
+++ coso/polls/models.py
@@ -17,6 +17,7 @@ class Candidate(models.Model):
     surname = models.CharField(max_length=50)
     birth_date = models.DateTimeField(blank=True, null=True)
     birth_place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=True, null=True)
+    nationality = models.ForeignKey(Place, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Election(models.Model):
@@ -35,19 +36,19 @@ class Result(models.Model):
     voting_result = models.DecimalField(max_digits=4, decimal_places=2)
 
 
-class PollInstitute(models.Model):
+class TrendSource(models.Model):
     name = models.CharField(max_length=50)
-    grade = models.DecimalField(max_digits=4,decimal_places=2)
+    grade = models.DecimalField(blank=True, null=True, max_digits=4,decimal_places=2)
 
 
-class Poll(models.Model):
+class Trend(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     date = models.DateTimeField(blank=True, null=True)
     election = models.ForeignKey(Election, on_delete=models.CASCADE)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     score = models.DecimalField(max_digits=4,decimal_places=2)
-    weight = models.DecimalField(max_digits=4,decimal_places=2)
-    poll_institute = models.ForeignKey(PollInstitute, on_delete=models.CASCADE)
+    weight = models.DecimalField(blank=True, null=True, max_digits=4,decimal_places=2)
+    trend_source = models.ForeignKey(TrendSource, on_delete=models.CASCADE)
 
 
 class Party(models.Model):
@@ -80,6 +81,7 @@ class PoliticalFunction(models.Model):
 
 
 class Role(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     beginning_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank = True, null=True)
     election = models.ForeignKey(Election, on_delete=models.CASCADE, blank=True, null=True)
