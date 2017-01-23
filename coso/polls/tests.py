c16e076e1221e2010a0dad1a9b292a371b2c9423
Melanie Berard <melanie.berard@student.ecp.fr>
2017-01-23 10:25:57 +0100

    Add the first tests to polls

--- coso/polls/tests.py
+++ coso/polls/tests.py
@@ -1,3 +1,18 @@
+from models import PoliticalFunction
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class PoliticalFunctionTests(TestCase):
+
+    def test_short_description(self):
+        """
+        Should return a short description of the field position
+        """
+        position = "European Commissioner for European Commissioner for Economic" \
+            + " and Monetary Affairs and the Euro|Economic and Financial Affairs, " \
+            + "European Commissioner for Taxation and Customs Union, Audit and Anti-Fr"
+        short_description = "European Commissioner for European Commissioner for Economic" \
+            + " and Monetary Affairs and the Euro|Ec..."
+        political_function = PoliticalFunction(position=position)
+        self.assertEquals(political_function.short_description, short_description)
