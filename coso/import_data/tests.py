5e6932c58d663fe9634ba8f09416256343fd5b74
kasimansour <kasramansouri94@gmail.com>
2017-02-08 22:20:23 +0100

    rebase with master

--- coso/import_data/tests.py
+++ coso/import_data/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-import views
+from views import save_to_database, get_tweets_by_day
 
 def test_aggregate_by_day(filtered_list, day):
-	return False
+	# save_to_database('2017-01-15', 'PrimaireGauche', 0)
+	get_tweets_by_day('2017-01-15', 'PrimaireGauche', 0)
+	print("Kasra fragile")
